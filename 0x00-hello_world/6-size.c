6f8ac622b84b984efb7cf46065471ce28777ea0b
simon kamau <muchara9@gmail.com>
2023-03-16 15:12:17 -0700

    C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size the various types on the computer
- * Return 0 (Success)
+ *main - A program that prints the size the various types on the computer
+ *Return 0 (Success)
  */
 int main(void)
 {
