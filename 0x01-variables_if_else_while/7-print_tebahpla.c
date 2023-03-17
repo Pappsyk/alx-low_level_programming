40f19710d0c2505690b97b054f0296d2af87058f
simon kamau <muchara9@gmail.com>
2023-03-17 14:44:59 -0700

    C - Variables if else

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
  * main - Prints the lowercase alphabet in reverse.
- *followed by a new line
  * Return: 0 (Success)
  */
 int main(void)
 {
-        char ch;
-
-        for (ch = 'z'; ch >= 'a'; ch--)
-        {       
-                putchar(ch);
-        }       
-        putchar("\n");
-        return (0);
-}       
+	char ch;
+	
+	for (ch = 'z'; ch >= 'a'; ch--)
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
