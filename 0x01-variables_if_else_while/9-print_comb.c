19b6f12853e5f634c8abcc23434b907f81ff9bcc
simon kamau <muchara9@gmail.com>
2023-03-17 15:14:47 -0700

    C - Variables if else

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9 and letters between.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int d;
+	for (d = '0'; d <= '9'; d++)
+	{
+	putchar(d);
+	if (d != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
