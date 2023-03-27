8c2d7a40824f90f47f332c2c37988c092c0af16a
simon kamau <muchara9@gmail.com>
2023-03-27 15:30:59 -0700

    C - More functions, more nested loops

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,8 +3,8 @@
 /**
  * print_array - a function that prints n elemnts of an array
  * @a: array name
- * @n: is the number of elements of the array to be printed
- * Return: a and n inputs
+ * @n: Number of elements of the array to be printed
+ * Return: void
  */
 void print_array(int *a, int n)
 {
