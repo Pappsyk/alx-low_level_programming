23c0d49c98440046e8f613d5a957025c0665ea85
simon kamau <muchara9@gmail.com>
2023-04-14 14:32:50 -0700

    C - More malloc, free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 #define ERR_MSG "Error"
-		
+
 /**
  * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
@@ -11,7 +11,7 @@
  * Return: 0 if a non-digit is found, 1 otherwise
  */
 int is_digit(char *s)
-{		
+{
 	int i = 0;
 
 	while (s[i])
@@ -22,7 +22,7 @@ int is_digit(char *s)
 	}
 	return (1);
 }
-		
+
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
@@ -31,14 +31,13 @@ int is_digit(char *s)
  */
 
 int _strlen(char *s)
-{		
+{
 	int i = 0;
 
 	while (s[i] != '\0')
 		i++;
 	return (i);
 }
-		
 /**
  * errors - handles errors for main
  */
@@ -47,7 +46,7 @@ void errors(void)
 	printf("Error\n");
 	exit(98);
 }
-		
+
 /**
  * main - multiplies two positive numbers
  * @argc: number of arguments
@@ -85,7 +84,7 @@ int main(int argc, char *argv[])
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
-	for (i = 0; i < len - 1; i++)	
+	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
 			a = 1;
